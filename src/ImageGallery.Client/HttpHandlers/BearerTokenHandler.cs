b7597f3d6ee270e6caf19ac68eafda23736491d0
Vinit Kadam <vinitkadam31@gmail.com>
2023-01-06 18:15:57 -0800

    Adding existing project

---
+++ src/ImageGallery.Client/HttpHandlers/BearerTokenHandler.cs
@@ -0,0 +1,106 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Client.HttpHandlers
+{
+    public class BearerTokenHandler : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BearerTokenHandler(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
+        {
+            _httpContextAccessor = httpContextAccessor ??
+                throw new ArgumentNullException(nameof(httpContextAccessor));
+
+            _httpClientFactory = httpClientFactory ??
+                throw new ArgumentNullException(nameof(httpClientFactory));
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await GetAccessTokenAsync();
+
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                request.SetBearerToken(accessToken);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var expiresAt = await _httpContextAccessor.HttpContext.GetTokenAsync("expires_at");
+
+            var expiresAtDateTimeOffset = DateTimeOffset.Parse(expiresAt, CultureInfo.InvariantCulture);
+
+            if (expiresAtDateTimeOffset.AddSeconds(-60).ToUniversalTime() > DateTime.UtcNow)
+            {
+                // no need to refresh, return access token
+                return await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            }
+
+            var idpClient = _httpClientFactory.CreateClient("IDPClient");
+            var discoveryResponse = await idpClient.GetDiscoveryDocumentAsync();
+
+            var refreshToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            var refreshResponse = await idpClient.RequestRefreshTokenAsync(new RefreshTokenRequest
+            {
+                Address = discoveryResponse.TokenEndpoint,
+                ClientId = "imagegalleryclient",
+                ClientSecret = "secret",
+                RefreshToken = refreshToken
+            });
+
+            //store the tokens
+            var updatedToken = new List<AuthenticationToken>();
+            updatedToken.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.IdToken,
+                Value = refreshResponse.IdentityToken
+            });
+
+            updatedToken.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.AccessToken,
+                Value = refreshResponse.AccessToken
+            });
+
+            updatedToken.Add(new AuthenticationToken
+            {
+                Name = OpenIdConnectParameterNames.RefreshToken,
+                Value = refreshResponse.RefreshToken
+            });
+
+            updatedToken.Add(new AuthenticationToken
+            {
+                Name = "expires_at",
+                Value = (DateTime.UtcNow + TimeSpan.FromSeconds(refreshResponse.ExpiresIn)).ToString("o", CultureInfo.InvariantCulture)
+            });
+
+            //Get Authenticate result, containing current principle & properties
+            var currentAuthenticateResult = await _httpContextAccessor.HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            //stores the updated tokens
+            currentAuthenticateResult.Properties.StoreTokens(updatedToken);
+
+            await _httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                currentAuthenticateResult.Principal,
+                currentAuthenticateResult.Properties);
+
+            return refreshResponse.AccessToken;
+        }
+    }
+}
---
+++ src/Marvin.IDP/Startup.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Logging;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Marvin.IDP
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var marvinIDPDataDBConnectionString = "Server=(localdb)\\mssqllocaldb;Database=MarvinIDPDataDB;Trusted_Connection=True;";
+
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var builder = services.AddIdentityServer()
+                //.AddInMemoryIdentityResources(Config.Ids)
+                //.AddInMemoryApiResources(Config.Apis)
+                //.AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users);
+
+            //builder.AddDeveloperSigningCredential();
+
+            builder.AddSigningCredential(LoadCertificateFromStore());
+            //IdentityModelEventSource.ShowPII = true;
+
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(marvinIDPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationAssembly));
+            });
+
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(marvinIDPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationAssembly));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            InitializeDatabase(app);
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+
+        public X509Certificate2 LoadCertificateFromStore()
+        {
+            string thumbprint = "3DF890786E7FD6B771C9A1D376A2E72EE56B0162";
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, true);
+                if(certCollection.Count == 0)
+                {
+                    throw new Exception("The specified certificate wasnt found");
+                }
+
+                return certCollection[0];
+            }
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using(var servicescope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                servicescope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = servicescope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                if(!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.Ids)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.Apis)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
